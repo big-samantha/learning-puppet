20e55f555f658a4b4cac8a33f6e4bd824e285269
Zachary Alex Stern <zacharyalexstern@gmail.com>
2012-08-13 19:04:51 +0000

    Foobarfoobar

--- break_ssh.pp
+++ break_ssh.pp
@@ -1,13 +1,21 @@
 # /root/learning-manifests/break_ssh.pp
 
-file { '/etc/ssh/sshd_config':
-  ensure => file,
+package { 'openssh-server':
+  ensure => present,
+  before => File['sshd_config']
+}
+
+file { 'sshd_config':
+  ensure => 'file',
+  path   => '/etc/ssh/sshd_config'
   mode   => 600,
   source => '/root/learning-manifests/sshd_config',
 }
 
 service { 'sshd':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/ssh/sshd_config'],
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  hasstatus  => true,
+  subscribe  => File['sshd_config'],
 }
